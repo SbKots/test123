020826bf6b3b007f0d28844cc1ed7159c352f3da
Art <kruckov3006@yandex.ru>
2026-05-08 18:36:51 +0300

    init commit

---
+++ src/Calculator.cs
@@ -0,0 +1,14 @@
+namespace ExamTraining;
+
+public class Calculator
+{
+    public int Sum(int a, int b)
+    {
+        return a + b;
+    }
+
+    public int Multiply(int a, int b)
+    {
+        return a * b;
+    }
+}
---
+++ src/ProjectInfo.cs
@@ -0,0 +1,7 @@
+namespace ExamTraining;
+
+public class ProjectInfo
+{
+    public string Name { get; set; } = "ExamTraining";
+    public string Version { get; set; } = "1.0.0";
+}
