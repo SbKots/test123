08152a6cdefe8252ec33d1f3ba9232e0b259eb23
Art <kruckov3006@yandex.ru>
2026-05-08 18:23:16 +0300

    init vs project

---
+++ src/Program.cs
@@ -0,0 +1,7 @@
+using ExamTraining;
+
+Calculator calculator = new Calculator();
+
+Console.WriteLine("Exam Git Training Project");
+Console.WriteLine($"2 + 3 = {calculator.Sum(2, 3)}");
+Console.WriteLine($"4 * 5 = {calculator.Multiply(4, 5)}");
